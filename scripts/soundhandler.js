177bb14557ce7560c83b3034c56735c7233d5389
btt-17 <btthanh.46@gmail.com>
2023-03-24 22:28:07 +0900

    add voice and mute

--- scripts/soundhandler.js
+++ scripts/soundhandler.js
@@ -1,8 +1,11 @@
 AFRAME.registerComponent('soundhandler', {
     tick: function () {
         var entity = document.querySelector('[sound]');
-        if (document.querySelector('a-marker').object3D.visible == true) {
-            entity.components.sound.playSound();
+        if (document.querySelector('a-marker').object3D.visible === true) {
+            console.log()
+            if(document.getElementById('voice').style.display !== 'none'){
+                entity.components.sound.playSound();
+            }
         } else {
             entity.components.sound.pauseSound();
         }
